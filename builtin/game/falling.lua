5bc6b0f331d21bf9e92bc338390150a2cc9298c7
zaoqi <zaomir@outlook.com>
2017-03-05 01:36:37 +0800

    Add minetest.spawn_falling_node(pos) (#5339)
    * Add minetest.spawn_falling_node(pos)
    
    * lua_api.txt: Add minetest.spawn_falling_node(pos)
    
    * Update minetest.spawn_falling_node(pos)

--- builtin/game/falling.lua
+++ builtin/game/falling.lua
@@ -153,6 +153,20 @@ function core.spawn_falling_node(pos)
 	return false
 end
 
+function core.spawn_falling_node(pos)
+	local node = core.get_node(pos)
+	if node.name == "air" or node.name == "ignore" then
+		return false
+	end
+	local obj = core.add_entity(pos, "__builtin:falling_node")
+	if obj then
+		obj:get_luaentity():set_node(node)
+		core.remove_node(pos)
+		return true
+	end
+	return false
+end
+
 local function drop_attached_node(p)
 	local n = core.get_node(p)
 	core.remove_node(p)
