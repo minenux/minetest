d2ab8c7d16574e259b7f15da2f5d16abeb92193d
luk3yx <luk3yx@users.noreply.github.com>
2019-02-16 15:56:10 +1300

    Grammar improvements  • https://github.com/minetest/minetest/commit/d6f2a1c4b8ab13cee92e2041b3410fe3548e88e6

--- builtin/mainmenu/pkgmgr.lua
+++ builtin/mainmenu/pkgmgr.lua
@@ -456,7 +456,7 @@ function pkgmgr.install_dir(type, path, basename, targetpath)
 				targetpath = core.get_modpath() .. DIR_DELIM .. clean_path
 			else
 				return nil,
-					fgettext("Install Mod: unable to find suitable foldername for modpack $1",
+					fgettext("Install Mod: Unable to find suitable foldername for modpack $1",
 					modfilename)
 			end
 		end
@@ -482,7 +482,7 @@ function pkgmgr.install_dir(type, path, basename, targetpath)
 			if targetfolder ~= nil and pkgmgr.isValidModname(targetfolder) then
 				targetpath = core.get_modpath() .. DIR_DELIM .. targetfolder
 			else
-				return nil, fgettext("Install Mod: unable to find real modname for: $1", modfilename)
+				return nil, fgettext("Install Mod: Unable to find real modname for: $1", modfilename)
 			end
 		end
 
@@ -518,7 +518,7 @@ function pkgmgr.install(type, modfilename, basename, dest)
 	if path == nil then
 		return nil,
 			fgettext("Install: file: \"$1\"", archive_info.name) .. "\n" ..
-			fgettext("Install: unsupported filetype \"$1\" or broken archive",
+			fgettext("Install: Unsupported filetype \"$1\" or broken archive",
 				archive_info.type)
 	end
 
