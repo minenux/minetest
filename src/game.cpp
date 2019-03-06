f1810e9d73cf3f5225b20799b7d2bee39eb266b0
xerox123official <ragulan90@gmail.com>
2019-03-06 19:08:56 +1300

    Display pitch angle in debug menu

--- src/game.cpp
+++ src/game.cpp
@@ -4384,6 +4384,7 @@ void Game::updateGui(const RunStats &stats, f32 dtime, const CameraOrientation &
 			<< ", " << (player_position.Z / BS)
 			<< "), yaw: " << (wrapDegrees_0_360(cam.camera_yaw)) << "° "
 			<< yawToDirectionString(cam.camera_yaw)
+			<< ", pitch: " << (wrapDegrees_180(cam.camera_pitch)) << "°"
 			<< ", seed: " << ((u64)client->getMapSeed());
 
 		if (runData.pointed_old.type == POINTEDTHING_NODE) {
